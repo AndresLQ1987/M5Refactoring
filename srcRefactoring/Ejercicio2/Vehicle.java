aaadbe6c1bad9880295a78512cf594b90a28ef98
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-05-10 16:20:01 -0700

    Ejercicio 2 Refactoring Terminado

---
+++ srcRefactoring/Ejercicio2/Vehicle.java
@@ -0,0 +1,33 @@
+package Ejercicio2;
+
+public class Vehicle {
+
+	private String model;
+	private String marca;
+	private Tipo categoria;
+	public static Tipo BASIC = Tipo.BASIC;
+	public static Tipo GENERAL = Tipo.GENERAL;
+	public static Tipo LUXE = Tipo.LUXE;
+
+	private enum Tipo {
+		BASIC, GENERAL, LUXE
+	}
+
+	public Vehicle(String model, String marca, Tipo cat) {
+		this.model = model;
+		this.marca = marca;
+		categoria = cat;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public Tipo getCategoria() {
+		return categoria;
+	}
+}
